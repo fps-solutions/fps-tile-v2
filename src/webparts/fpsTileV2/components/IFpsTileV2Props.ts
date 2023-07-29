e2cd1855e3d77781f4e6af4d7840bb2597dca9ed
mikezimm <49648086+mikezimm@users.noreply.github.com>
2023-07-29 00:23:01 -0400

    base yo web part

---
+++ src/webparts/fpsTileV2/components/IFpsTileV2Props.ts
@@ -0,0 +1,7 @@
+export interface IFpsTileV2Props {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/fpsTileV2/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IFpsTileV2WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'FpsTileV2WebPartStrings' {
+  const strings: IFpsTileV2WebPartStrings;
+  export = strings;
+}
